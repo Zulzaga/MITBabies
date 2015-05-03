d9b536d8ae0dd4af01cbbc3ad809250006974819
Zulsar Batmunkh <zulsar@mit.edu>
2015-05-03 12:42:06 -0400

    Home Page added

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,5 +1,29 @@
-$(document).on('click', '#jobs_page', function(event) {
-	event.preventDefault();
-	loadCompaniesPage(res);
+$(document).ready(function() {
+
+	
+	$("#jobCreation").submit(function() {
+		console.log($("#title").val());
+	})
+
+	function scrollToID(id, speed){
+		var offSet = 100;
+		var targetOffset = $(id).offset().top - offSet;
+		var mainNav = $('#small-menu');
+		$('html,body').animate({scrollTop:targetOffset}, speed);
+		if (mainNav.hasClass("open")) {
+			mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+			mainNav.removeClass("open");
+		}
+	}
+
+	$('.scroll-link').on('click', function(event) {
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	})
+
+	$('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
 });
 
