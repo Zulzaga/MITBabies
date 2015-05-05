85190934e575ee34294d2d956d0c50429b68ff59
Zulsar Batmunkh <zulsar@mit.edu>
2015-05-05 15:45:56 -0400

    Navbar updated

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -442,6 +442,22 @@ $(document).ready(function() {
         }, 750);
     }
 
+    $('.scroll-link').on('click', function(event) {
+		event.preventDefault();
+		var sectionID = $(this).attr("data-id");
+		scrollToID('#' + sectionID, 750);
+	})
+	function scrollToID(id, speed){
+		var offSet = 100;
+		var targetOffset = $(id).offset().top - offSet;
+		var mainNav = $('#small-menu');
+		$('html,body').animate({scrollTop:targetOffset}, speed);
+		if (mainNav.hasClass("open")) {
+			mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+			mainNav.removeClass("open");
+		}
+	}
+
     var formatTime = function(time){
     	var t = new Date(time);
     	var hours = t.getHours();
